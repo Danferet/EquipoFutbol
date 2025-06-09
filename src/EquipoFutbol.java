26601273ac9f53541b42b2b46ffb56b7aca83fa1
Daniel Sánchez Aguado <Majydx@hotmail.com>
2025-06-09 22:19:01 +0200

    2 subida
    Comentando el código

--- src/EquipoFutbol.java
+++ src/EquipoFutbol.java
@@ -1,28 +1,49 @@
 import java.util.ArrayList;
 
+//clase princial que tendrá un nombre, un entrenadro 1:1 y una lista de jugadores 1:N
 public class EquipoFutbol {
 
     private String nombre;
     private Entrenador entrenador;
     private ArrayList<Jugador> listaJugadores;
 
+    //Constructor que pasa por parámetros el nombre del equipo y el entrenador
     public EquipoFutbol(String nombre, Entrenador entrenador){
         this.nombre = nombre;
         this.entrenador = entrenador;
+        //La lista se inicializa, pero no se pasa la lista completa por parámetro.
         this.listaJugadores = new ArrayList<>();
 
     }
 
+    //Con este metodo pasamos jugadores a la lista.
+    //Retorna un void, no se guarda en ningna variable, solo hace su trabajo y punto.
+    //Por convención se llaman add... en este Caso addJugador.
+    //Por parámetros pasamos una instancia de la clase jugador.
+    //Dentro del metodo, cogemos la lista y con el .add que tienen los ArrayList<> le pasamos el jugador
     public void addJugador(Jugador jugador){
 
         listaJugadores.add(jugador);
 
     }
 
+
+    //Metodos getters y setters
+
+    //Metodo getter, estructura: palabra reservada public; dato que devuelve;
+    //Nombre del metodo -> get... en este caso getNombre;
+    //Finalmente los parentesis vacíos (), ya que no reciben nada por parámetros;
+    //Dentro del metodo tendremos la palabra reservada return seguida del atribudo que devuelve, en este caso nombre;
     public String getNombre() {
         return nombre;
     }
 
+
+    //Metodo setter, estructura: palabra reservada public; void, pues no devuelve nada, solo hace un cambio;
+    //Nombre del metodo -> set... en este caso setNombre;
+    //Finalmente, los paréntesis:
+    //En este caso con el tipo de dato del atributo que vamos a cambiar y un nombre para la variable.
+    //Dentro del metodo, igual que en el constructor para el atributo: this.nombredelatributo = nombredelatributo.
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -42,6 +63,10 @@ public class EquipoFutbol {
     public void setListaJugadores(ArrayList<Jugador> listaJugadores) {
         this.listaJugadores = listaJugadores;
     }
+
+    //Metodo to String: metodo predefinido en java. Se llama con public String toString(){}
+    //Dentro de las llaves podemos hacer que nos retorne los datos de la clase como notros queramos,
+    //pasándolo luego por el return como un tipo cadena.
 
     @Override
     public String toString() {
--- src/Jugador.java
+++ src/Jugador.java
@@ -2,9 +2,16 @@ public class Jugador extends Persona{
 
     private String piernaBuena;
 
+    //En el constructor tenemos que poner todos los atributos que queramos que tenga el Jugador,
+    //Incluidos los que no tiene aquí, que hereda de la clase padre.
+    //nombre, edad y nacionalidad los hereda de la clase padre, y piernaBuena es propia.
     public Jugador(String nombre, int edad, String nacionalidad, String piernaBuena){
 
+        //super hará referencia al constructor de la clase padre
+        // y guardará las variables en los atributos como si estuvieran en esta misma clase
         super(nombre, edad, nacionalidad);
+
+        //Agregamos además el atributo propio
         this.piernaBuena = piernaBuena;
 
     }
@@ -17,6 +24,13 @@ public class Jugador extends Persona{
         this.piernaBuena = piernaBuena;
     }
 
+    //Un metodo de lógica de programa simple para hacer una prueba. Estructura:
+    //palabra resevada public; dato que devuelve, en este caso, String; nombre del metodo;
+    //Paréntesis con parámetros, en este caso recibe un String.
+
+    //Dentro del metodo obligatorio el return, ya que devuelve un dato.
+    //En este caso el jugador saluda a otra "quien sea". Obtenemos el nombre del jugador que utilice el metodo
+    //con su respectivo metodo getNombre, nuestra frase saluda a y seguidamente la cadena que pasamos por parámetro
     public String saludar(String nombre){
 
         return getNombre() + " saluda a " + nombre + ".";
